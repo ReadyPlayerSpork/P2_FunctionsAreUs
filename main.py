86ace44a721f5e34879607f92d1f2b13d0236f38
Luke <lukeaaronmiller@gmail.com>
2025-02-27 12:49:39 -0700

    Main program file + adjustments
    Adds the main program file to put everything together (wip). Renames some of the function files to be easier to refer to in the main file.

---
+++ main.py
@@ -0,0 +1,11 @@
+import function1and2
+import function3Part1
+import function_4
+import function_5
+
+name = function1and2.intro()
+print(name)
+
+menuChoice = function1and2.menu()
+print(menuChoice)
+
