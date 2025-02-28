d23a0d6132cb1e49420663ac9e4f8ceedfc6e349
skmorty <sethkmorty@gmail.com>
2025-02-27 23:25:41 -0700

    Seth-added-function-3-to-main
    I imported the functions fro "function3Final". Then wrote the code that if a user selects "1" or "choose team" it runs the functions to show a menu of teams and assign the selected ones to home team and away team.
    
    I also modified the functions on function 1 + 2
    intro function : rstrip so that it doesn't keep trace of spaces if one is entered at the end of the name
    
    menu: int() just so that the choice doesn't come back as a string.

--- function3Final.py
+++ function3Final.py
@@ -37,30 +37,30 @@ def chooseTeam (teams,homeTeam,oppTeam, userChoice,available_teams):
 
 # code for main file of code 
 # create teams list, set up variables
-teams = ["BYU", "UVU" , "USU"]
-homeTeam = None 
-oppTeam = None
-userChoice = 0
-available_teams = teams
-# run until both a home team and a opposing team have been choosen
-while homeTeam == None or oppTeam == None:
-    try : 
-        # display choices
-        print ("Input 1 to select the home team")
-        print ("Input 2 to select the opposing team")
-        # get input for choice
-        userChoice = int(input("Enter your input:"))
-        # if 1st option then assign the team they choose to home team
-        if userChoice == 1 : 
-            # home team  = return of they team of choice
-            homeTeam = chooseTeam(teams,homeTeam,oppTeam,userChoice,available_teams)
-        # if 2nd option then assign the team they choose to oppposing team
-        elif userChoice == 2 :
-            # opposing team  = return of they team of choice
-            oppTeam = chooseTeam(teams,homeTeam,oppTeam,userChoice,available_teams)
-        else :
-            # if a number is entered other than 1 or 2 print error
-            print("Please enter a 1 or 2.")
-    except ValueError : 
-        # if the input is not an integer then print error
-        print ("Please enter a number, 1 or 2.")
+# teams = ["BYU", "UVU" , "USU"]
+# homeTeam = None 
+# oppTeam = None
+# userChoice = 0
+# available_teams = teams
+# # run until both a home team and a opposing team have been choosen
+# while homeTeam == None or oppTeam == None:
+#     try : 
+#         # display choices
+#         print ("Input 1 to select the home team")
+#         print ("Input 2 to select the opposing team")
+#         # get input for choice
+#         userChoice = int(input("Enter your input:"))
+#         # if 1st option then assign the team they choose to home team
+#         if userChoice == 1 : 
+#             # home team  = return of they team of choice
+#             homeTeam = chooseTeam(teams,homeTeam,oppTeam,userChoice,available_teams)
+#         # if 2nd option then assign the team they choose to oppposing team
+#         elif userChoice == 2 :
+#             # opposing team  = return of they team of choice
+#             oppTeam = chooseTeam(teams,homeTeam,oppTeam,userChoice,available_teams)
+#         else :
+#             # if a number is entered other than 1 or 2 print error
+#             print("Please enter a 1 or 2.")
+#     except ValueError : 
+#         # if the input is not an integer then print error
+#         print ("Please enter a number, 1 or 2.")
