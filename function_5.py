5183cd746e2f9735163ac61ff8ba386b40513bac
Luke <lukeaaronmiller@gmail.com>
2025-02-28 12:41:35 -0700

    Project Completed - Rough Draft
    Pulls all of the functions together and creates a functional working program. Many edits were made to make this possible, so take a look at the changed code in detail before approving push to main.

--- function_5.py
+++ function_5.py
@@ -5,12 +5,12 @@
 # Using parameters, print final record
 # calculate whether the team needs to practice
 
-def results(homeTeamName, totalWins, num_games):
-    print(f"Final season record: {homeTeamName} {totalWins}-{num_games-totalWins}") # printing results
+def results(homeTeamName, totalWins, totalLosses):
+    print(f"Final season record: {homeTeamName} {totalWins}-{totalLosses}") # printing results
     # printing whether or not the team needs to practice
-    if totalWins >= num_games * .75: 
+    if totalWins >= (totalWins+totalLosses) * .75: 
         print("Qualified for the NCAA Women's Soccer Tournament")
-    elif totalWins >= num_games * .5:
+    elif totalWins >= (totalWins+totalLosses) * .5:
         print("You had a good season")
     else:
         print("Your team needs to practice!")
